bbbe37326dfab9d57930e2df4fc8ec61e3e64210
Claus Bové <clbo@kea.dk>
2017-04-18 16:50:32 +0200

    Create ExerciseCountWords.java

---
+++ Exercises/ExerciseCountWords.java
@@ -0,0 +1,18 @@
+// Counts the number of words in Hamlet.
+
+// 1. Import the libaries for the Scanner and the File Classes
+
+public class CountWords 
+{
+   public static void main(String[] args)throws FileNotFoundException 
+   {
+        // 2. create a new File object containing hamlet.txt
+        
+      
+        // 3. Loop through the text using a while loop and count all the words (Tokens)
+
+      
+      // 4. print out the total amount of words in the file
+      
+   }
+}
