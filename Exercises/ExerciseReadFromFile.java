3dc890e2b43c99a581e9b1c08fd9ec09cfb8a3fe
Claus Bové <clbo@kea.dk>
2017-04-18 16:43:38 +0200

    Create ExerciseReadFromFile.java

---
+++ Exercises/ExerciseReadFromFile.java
@@ -0,0 +1,24 @@
+// 1. import the File class
+
+public class ExerciseReadFromFile 
+{
+   public static void main(String[] args) 
+   {
+      // 2. Create a File Object that reads from a file on you HD
+
+      
+      // 3. Create a program that reads from a file
+      //    And uses all these methods
+      
+      // canRead()
+      // delete()
+      // exists()
+      // getAbsolutePath()
+      // getName()
+      // isDirectory()
+      // isFile()
+      // length()
+      // renameTo(file)
+      
+   }
+}
