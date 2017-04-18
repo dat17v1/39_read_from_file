792722b8099d2d447a1860782dc80f9c93b01cd5
clbokea <clbo@kea.dk>
2017-04-18 20:54:32 +0200

    init

--- Exercises/ExerciseCountWords.java
+++ Exercises/ExerciseCountWords.java
@@ -4,12 +4,14 @@
 
 public class CountWords 
 {
-   public static void main(String[] args)throws FileNotFoundException 
+   public static void main(String[] args) 
    {
-        // 2. create a new File object containing hamlet.txt
+      // 2. Create a new File object that contains the file hamlet.txt
+        
+      // 2. Create a new Scanner object that reads from the file object
         
       
-        // 3. Loop through the text using a while loop and count all the words (Tokens)
+      // 3. Loop through the text using a while loop and count all the words (Tokens)
 
       
       // 4. print out the total amount of words in the file
