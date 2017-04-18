b9ffd253ba0c5b776396cc8ed9193ec408834d0a
clbokea <clbo@kea.dk>
2017-04-18 21:39:02 +0200

    readfromdb

---
+++ Exercises/ExerciseReadFromDB.java
@@ -0,0 +1,14 @@
+public class ExerciseReadFromDB
+{
+    public static void main(String[] args)
+    {
+        //1. Create a text file containing persons with a new person on each line
+        // Name lastname age cpr 
+        // put in at least 3 persons
+
+        // 2 Read all persons from the file and write it to the console
+
+        // 3. Read all persons from the file and write it to an Array or ArrayList
+    }
+
+}
