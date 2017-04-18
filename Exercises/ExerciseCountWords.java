c1612a56fdd5db01edba05f01f5fd3fd609f7681
clbokea <clbo@kea.dk>
2017-04-18 21:01:40 +0200

    try/catch

--- Exercises/ExerciseCountWords.java
+++ Exercises/ExerciseCountWords.java
@@ -8,11 +8,9 @@ public class CountWords
    {
       // 2. Create a new File object that contains the file hamlet.txt        
       // 3. Create a new Scanner object that reads from the file object
-          // 3a. in order to do this you will need a try catch clause
-        
+          // 3a. in order to do this you will need a try catch clause     
       
       // 4. Loop through the text using a while loop and count all the words (Tokens)
-
       
       // 5. print out the total amount of words in the file
       
